d6f7cb3653f90017af0f5978ea0659e9e3658f6d
Anders Wallin <anders.e.e.wallin@gmail.com>
2010-03-31 14:33:59 +0000

    document drop-cutter vertex and facet tests

--- manual.h
+++ manual.h
@@ -51,15 +51,18 @@
  */
 
 /*! \page p2 OpenCAMLib developer manual
+ * 
  * \section intro_dev Introduction
  * This section of the manual describes the computational geometry and algorithms used in OpenCAMLib.
  * Three-dimensional points and vectors are denoted with <b>bold</b>.
+ * 
  * \section Drop-Cutter
  * The drop cutter algorithm drops a cutter, positioned at a predefined (x,y) location
  * down along the z-axis until it makes contact with a triangle.
  * The algorithm is split into three parts, testing the cutter separately against the 
  * three vertices, three edges, and the facet of the triangle.
  * In general the vertex test is trivial, the facet test easy, and the edge test is the most difficult.
+ * 
  * \subsection dc-gen General
  * A cutter-location (cl) point is a point where the cutter tip can be located when in contact
  * with the surface, but without interfering with the STL-surface.
@@ -67,16 +70,34 @@
  * The user specifies the (cl.x, cl.y) coordinates of the cutter, and an initial value cl.z for the cl z-coordinate. 
  * The task for the vertex, facet, and edge tests is to update the height of the cutter cl.z. When each vertex, edge,
  * and facet of each triangle under the cutter positioned at (cl.x, cl.y) has been tested, the resulting cl.z is the 
- * correct and final height of the cutter we want. 
- * \subsection cylcutter Cylindrical endmills (Flat)
- * \subsubsection cylvert Vertex test
+ * correct and final height of the cutter we want. If no test finds a contact between the cutter and a triangle, then 
+ * cl.z is left unmodified.
+ * 
+ * Milling cutters supported by opencamlib
+ *      - Cylindrical
+ *      - Spherical
+ *      - Toroidal
+ *      
+ * \subsection verttest Vertex test
+ * Milling cutters are symmetrical around their axis of rotation (the z-axis in 3-axis milling), so the vertext
+ * test only depends on the distance between the cutter axis and the test-vertex.
  * Given <b>cl</b> and a vertex <b>p</b> to test, calculate the distance d in the xy-plane from <b>cl</b> to <b>p</b>: 
  * \f[
- * d = |cl - p|= \sqrt{(cl.x-p.x)^2+(cl.y-p.y)^2}
+ * d = |cl - p| = \sqrt{(cl.x-p.x)^2+(cl.y-p.y)^2}
  * \f]
- * If \f$d <= diameter/2\f$ the cutter will make contact with the vertex, and we can set \f$cl.z = p.z\f$ .
  * If \f$d > diameter/2\f$ the cutter will not contact the vertex and we return without modifying cl.z.
- * \subsubsection cylfacet Facet test
+ * If \f$d <= diameter/2\f$ the cutter will make contact with the vertex.
+ * 
+ *      - Cylindrical cutters are flat we can set \f$cl.z = p.z\f$ 
+ *      - For a Spherical cutter (see FigureX) \f$h = r - \sqrt{ r^2 - d^2 }\f$ and we set \f$cl.z = p.z - h\f$
+ *      - For Toroidal cutters:
+ *          - If \f$d < r_1\f$ we are in the cylindrical part of the cutter and set \f$cl.z = p.z\f$
+ *          - If \f$r_1 < d < r\f$ we are in the toroidal part and \f$ h = r_2 - \sqrt{ r_2^2 - (d-r_1)^2 }\f$ and then \f$cl.z = p.z - h\f$
+ * 
+ * (TODO: insert image of vertex test here)
+ * 
+ * 
+ * \subsection facettest Facet test
  * Call the three vertices of the triangle <b>p1</b>, <b>p2</b>, and <b>p3</b>.
  * The facet test is really a test against the whole plane which contains the triangle facet.
  * First calculate the normal <b>n</b> of the facet, and normalize its length to one. The equation for the
@@ -84,12 +105,17 @@
  * \f[
  * a*x + b*y + c*z + d = 0
  * \f]
- * where the coefficients \f$(a, b, c) = n\f$. The constant d can be calculated by inserting one vertex 
- * into the formula of the plane: \f$d = -n\cdot p1\f$ .
+ * where the coefficients \f$(a, b, c) = \bar{n}\f$. The constant d can be calculated by inserting one vertex 
+ * into the formula of the plane: \f$d = -\bar{n}\cdot \bar{p_1}\f$.
+ * 
+ * Also calculate and normalize the xy-projection of the normal \f$\bar{n_{xy}}\f$. 
+ * Note that vertical facets with n.z = 0 are a special case which are ignored in the drop-cutter algorithm.
+ * 
+ * \subsubsection cylfacettest Cylindrical cutter facet test
  * The contact point with the plane will lie on the periphery of the cutter, a
- * distance \f$diameter/2\f$ from \f$(cl.x, cl.y)\f$ in the direction \f$-n\f$, so the xy-coordinates of the cc-point are given by:
+ * distance \f$diameter/2\f$ from \f$(cl.x, cl.y)\f$ in the direction \f$-\bar{n_{xy}}\f$, so the xy-coordinates of the cc-point are given by:
  * \f[
- * (cc.x, cc.y) = (cl.x, cl.y) - (diameter/2)*n
+ * (cc.x, cc.y) = (cl.x, cl.y) - r*\bar{n_{xy}}
  * \f]
  * This cc-point lies on the plane, but not necessarily in the facet of the triangle. 
  * If the cc-point is contained within the triangle (in the xy-plane), we can calculate its z-coordinate by
@@ -97,29 +123,73 @@
  * \f[
  * cc.z = (1/c)*(-d-a*cc.x-b*cc.y)
  * \f] 
- * Since the cylindrical endmill is flat, we can also set \f$cl.z = cc.z\f$. Done.
- * \subsubsection cyledge Edge test
+ * Since the cylindrical endmill is flat, we can also set \f$cl.z = cc.z\f$. Note division by \f$c\f$ which requires \f$c = n.z \neq 0\f$
+ * 
+ * \subsubsection sphfacettest Spherical cutter facet test
+ * The center of the sphere is located a distance \f$diameter/2\f$  from \f$(cl.x, cl.y)\f$ in the direction of \f$-\bar{n}\f$. 
+ * We can find out the xy-compontents of the cc point with:
+ * \f[
+ * \bar{cc} = \bar{cl} - r*\bar{n}
+ * \f]
+ * If this point lies within the facet of the triangle we locate the point on the plane by setting cc.z on the plane:
+ * \f[
+ * cc.z = (1/c)*(-d-a*cc.x-b*cc.y)
+ * \f] 
+ * The tool-tip z-coordinate is now given by \f$cl.z = cc.z + r*\bar{n}\cdot\hat{z}  - r\f$
+ * 
+ * \subsubsection torfacettest Toroidal cutter facet test
+ * define a vector which points from the cc-point to the center of the toroid:
+ * \f[
+ * \bar{R} = -r_2*\bar{n} - r_1* \bar{n_{xy}}
+ * \f]  
+ * Now the xy-coordinates of the cc-point are given by
+ * \f[
+ * \bar{cc} = \bar{cl} + \bar{R}
+ * \f] 
+ * now check if the cc-point lies in the facet of the plane. If so, the z-coordinate of the cc-point is calculated:
+ * \f[
+ * cc.z = (1/c)*(-d-a*cc.x-b*cc.y)
+ * \f]  
+ * And the tool-tip will be located at \f$cl.z = cc.z + r_2*n.z - r_2\f$
+ * 
+ * \subsection edgetest Edge test
  * Explain the edge-test here...
- * \subsection ballcutter Spherical endmills (Ball-nose)
- * \subsubsection ballvert Vertex test
- * \subsubsection ballfacet Facet test
- * \subsubsection balledge Edge test
- * \subsection bullcutter Toroidal endmills (Bull-nose)
- * \subsubsection bullvert Vertex test
- * \subsubsection bullfacet Facet test
- * \subsubsection bulledge Edge test
+ * \subsubsection cyledge Cylindrical cutter edge test
+ * \subsubsection balledge Spherical cutter edge test
+ * \subsubsection bulledge Toroidal cutter edge test
+ * 
+ * 
  * \subsection optdc Optimizing drop-cutter
  * \subsubsection kdtree Using a kd-tree for finding triangles under the cutter
+ * 
+ * 
+ * 
  * \section Z-slice
  * An algorithm for 'slicing' an STL surface at a defined Z-coordinate. Produces waterline type paths. 
  * These contours can be used as a starting point for area-clearing algorithms in roughing operations.
+ * 
+ * 
  * \section cutsim Cutting Simulation
  * Describe cutting simulation here (octree etc).
+ * 
+ * 
  * \section References
  * \subsection Books
  * \li O'Rourke, Computational Geometry in C.
  * \li deBerg, Computational Geometry: Algorithms and Applications
+ * 
  * \subsection Papers  
+ * \li Chuang2002  http://dx.doi.org/10.1007/PL00003965 "A Reverse Engineering Approach to Generating Interference-Free Tool 
+ * Paths in Three-Axis Machining from Scanned Data of Physical Models"
+ *      - this paper contains the basics of drop-cutter
+ * \li Hwang1998 http://dx.doi.org/10.1016/S0010-4485(98)00021-9 
+ *      - DropCutter for flat, filleted and ball-nose cutters. Edge test with iterative solution. 
+ * \li Yau2004 http://dx.doi.org/10.1080/00207540410001671651 
+ *      - DropCutter for a general APT-style cutter. This includes cutters with pointed ends and tapered cutters. 
+ *      - kd-tree for triangle search
+ * \li Chuang2005 http://dx.doi.org/10.1016/j.cad.2004.10.005 
+ *      - presents a Push-cutter idea for calculating z-level (waterline) paths. 
+ * 
 */
 
 
