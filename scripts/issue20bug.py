fb1aa51cf25154f76855660fe4c327dfee7395e9
Anders Wallin <anders.e.e.wallin@gmail.com>
2010-06-03 05:15:29 +0000

    files that reproduce issue20

---
+++ scripts/issue20bug.py
@@ -0,0 +1,43 @@
+import ocl
+import camvtk
+import issue20data
+
+if __name__ == "__main__":  
+    s = ocl.STLSurf()
+    triangles = issue20data.trilist
+    
+    for t in triangles:
+        s.addTriangle(t)
+    
+    print ocl.revision()    
+    # define a cutter
+    cutter = ocl.CylCutter(3)
+    #cutter = ocl.BallCutter(3)
+    #cutter = ocl.BullCutter(3,0.2)
+
+    pdf = ocl.PathDropCutterFinish(s)   # create a pdf-object for the surface s
+    pdf.setCutter(cutter)               # set the cutter
+    pdf.minimumZ = -1                   # set the minimum Z-coordinate, or
+                                        # "floor" for drop-cutter
+                                        
+    path = ocl.Path()                   # create an empty path object 
+    # add a line to the path
+    path.append(ocl.Line(ocl.Point(0, 0.098, 0), ocl.Point(4, 0.098, 0)))
+
+    # set the path for pdf
+    pdf.setPath( path )
+    
+    pdf.run()                   # run drop-cutter on the path
+    
+    clp = pdf.getCLPoints()     # get the cl-points from pdf
+    ccp = pdf.getCCPoints()     # get the cc-points from pdf
+
+    print 'first point ', clp[0], '   ( z should be at z5 )'
+    
+    # visualize things with VTK
+    myscreen = camvtk.VTKScreen()
+    myscreen.addActor( camvtk.PointCloud( pointlist=clp, collist=ccp ) )
+    myscreen.addActor( camvtk.PointCloud( pointlist=ccp, collist=ccp ) )
+    vtkSurf = camvtk.STLSurf(filename=None, triangleList=triangles )
+    myscreen.addActor(vtkSurf)
+    myscreen.iren.Start()
---
+++ scripts/issue20data.py
@@ -0,0 +1,79 @@
+import ocl
+
+trilist = []
+trilist.append(ocl.Triangle(ocl.Point(2.91337, 1, 3.93857),
+                            ocl.Point(2.89783, -7.09741e-016, 4.07462), 
+                            ocl.Point(4, -9.79685e-016, 3)))
+trilist.append(ocl.Triangle(ocl.Point(2.91337, 1, 3.93857), 
+                            ocl.Point(4,0.673874, 2.92334), 
+                            ocl.Point(4, 1, 2.82843)))
+trilist.append(ocl.Triangle(ocl.Point(2.91337, 1, 3.93857), 
+                            ocl.Point(4,0.339111, 2.98077), 
+                            ocl.Point(4, 0.673874, 2.92334)))
+trilist.append(ocl.Triangle(ocl.Point(2.91337, 1, 3.93857), ocl.Point(4,
+-9.79685e-016, 3), ocl.Point(4, 0.339111, 2.98077)))
+trilist.append(ocl.Triangle(ocl.Point(1.53379, 1, 4.65268),
+ocl.Point(1.521, -3.72525e-016, 4.76304), ocl.Point(2.89783, -7.09741e-016,
+4.07462)))
+trilist.append(ocl.Triangle(ocl.Point(1.53379, 1, 4.65268),
+ocl.Point(2.89783, -7.09741e-016, 4.07462), ocl.Point(2.91337, 1, 3.93857)))
+trilist.append(ocl.Triangle(ocl.Point(0, 0.669179, 4.95502),
+ocl.Point(1.53379, 1, 4.65268), ocl.Point(-1.19986e-015, 1, 4.89898)))
+trilist.append(ocl.Triangle(ocl.Point(0, 0.335345, 4.98874),
+ocl.Point(1.53379, 1, 4.65268), ocl.Point(0, 0.669179, 4.95502)))
+trilist.append(ocl.Triangle(ocl.Point(3.06152e-016, -7.4983e-032, 5),
+ocl.Point(1.521, -3.72525e-016, 4.76304), ocl.Point(1.53379, 1, 4.65268)))
+trilist.append(ocl.Triangle(ocl.Point(3.06152e-016, -7.4983e-032, 5),
+ocl.Point(1.53379, 1, 4.65268), ocl.Point(0, 0.335345, 4.98874)))
+trilist.append(ocl.Triangle(ocl.Point(2, 1, 2.44949), ocl.Point(0, 1,
+0), ocl.Point(-1.19986e-015, 1, 4.89898)))
+trilist.append(ocl.Triangle(ocl.Point(2, 1, 2.44949),
+ocl.Point(-1.19986e-015, 1, 4.89898), ocl.Point(1.53379, 1, 4.65268)))
+trilist.append(ocl.Triangle(ocl.Point(2, 1, 2.44949), ocl.Point(1.53379,
+1, 4.65268), ocl.Point(2.91337, 1, 3.93857)))
+trilist.append(ocl.Triangle(ocl.Point(2, 1, 2.44949), ocl.Point(2.91337,
+1, 3.93857), ocl.Point(4, 1, 2.82843)))
+trilist.append(ocl.Triangle(ocl.Point(2, 1, 2.44949), ocl.Point(4, 1,
+2.82843), ocl.Point(4, 1, 0)))
+trilist.append(ocl.Triangle(ocl.Point(2, 1, 2.44949), ocl.Point(4, 1,
+0), ocl.Point(0, 1, 0)))
+trilist.append(ocl.Triangle(ocl.Point(4, 0.5, 1.5), ocl.Point(4,
+-9.79685e-016, 3), ocl.Point(4, 0, 0)))
+trilist.append(ocl.Triangle(ocl.Point(4, 0.5, 1.5), ocl.Point(4, 0, 0),
+ocl.Point(4, 1, 0)))
+trilist.append(ocl.Triangle(ocl.Point(4, 0.5, 1.5), ocl.Point(4, 1, 0),
+ocl.Point(4, 1, 2.82843)))
+trilist.append(ocl.Triangle(ocl.Point(4, 0.5, 1.5), ocl.Point(4, 1,
+2.82843), ocl.Point(4, 0.673874, 2.92334)))
+trilist.append(ocl.Triangle(ocl.Point(4, 0.5, 1.5), ocl.Point(4,
+0.673874, 2.92334), ocl.Point(4, 0.339111, 2.98077)))
+trilist.append(ocl.Triangle(ocl.Point(4, 0.5, 1.5), ocl.Point(4,
+0.339111, 2.98077), ocl.Point(4, -9.79685e-016, 3)))
+trilist.append(ocl.Triangle(ocl.Point(2, 0, 2.5),
+ocl.Point(3.06152e-016, -7.4983e-032, 5), ocl.Point(0, 0, 0)))
+trilist.append(ocl.Triangle(ocl.Point(2, 0, 2.5), ocl.Point(4,
+-9.79685e-016, 3), ocl.Point(2.89783, -7.09741e-016, 4.07462)))
+trilist.append(ocl.Triangle(ocl.Point(2, 0, 2.5), ocl.Point(2.89783,
+-7.09741e-016, 4.07462), ocl.Point(1.521, -3.72525e-016, 4.76304)))
+trilist.append(ocl.Triangle(ocl.Point(2, 0, 2.5), ocl.Point(1.521,
+-3.72525e-016, 4.76304), ocl.Point(3.06152e-016, -7.4983e-032, 5)))
+trilist.append(ocl.Triangle(ocl.Point(2, 0, 2.5), ocl.Point(4, 0, 0),
+ocl.Point(4, -9.79685e-016, 3)))
+trilist.append(ocl.Triangle(ocl.Point(2, 0, 2.5), ocl.Point(0, 0, 0),
+ocl.Point(4, 0, 0)))
+trilist.append(ocl.Triangle(ocl.Point(0, 0.5, 2.5),
+ocl.Point(-1.19986e-015, 1, 4.89898), ocl.Point(0, 1, 0)))
+trilist.append(ocl.Triangle(ocl.Point(0, 0.5, 2.5), ocl.Point(0,
+0.669179, 4.95502), ocl.Point(-1.19986e-015, 1, 4.89898)))
+trilist.append(ocl.Triangle(ocl.Point(0, 0.5, 2.5), ocl.Point(0,
+0.335345, 4.98874), ocl.Point(0, 0.669179, 4.95502)))
+trilist.append(ocl.Triangle(ocl.Point(0, 0.5, 2.5),
+ocl.Point(3.06152e-016, -7.4983e-032, 5), ocl.Point(0, 0.335345, 4.98874)))
+trilist.append(ocl.Triangle(ocl.Point(0, 0.5, 2.5), ocl.Point(0, 0, 0),
+ocl.Point(3.06152e-016, -7.4983e-032, 5)))
+trilist.append(ocl.Triangle(ocl.Point(0, 0.5, 2.5), ocl.Point(0, 1, 0),
+ocl.Point(0, 0, 0)))
+trilist.append(ocl.Triangle(ocl.Point(4, 1, 0), ocl.Point(0, 0, 0),
+ocl.Point(0, 1, 0)))
+trilist.append(ocl.Triangle(ocl.Point(4, 1, 0), ocl.Point(4, 0, 0),
+ocl.Point(0, 0, 0)))
