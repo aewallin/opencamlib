9e7f2bfbf87bcf4a282a40034163a664b2172223
Anders Wallin <anders.e.e.wallin@gmail.com>
2010-07-05 16:50:59 +0000

    cl-filter

---
+++ src/clfilter.h
@@ -0,0 +1,75 @@
+/*  $Id:  $
+ * 
+ *  Copyright 2010 Anders Wallin (anders.e.e.wallin "at" gmail.com)
+ *  
+ *  This file is part of OpenCAMlib.
+ *
+ *  OpenCAMlib is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  OpenCAMlib is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with OpenCAMlib.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef CLFILTER_H
+#define CLFILTER_H
+
+#include <boost/python.hpp>
+
+#include <iostream>
+#include <string>
+#include <list>
+#include "point.h"
+#include "clpoint.h"
+
+
+namespace ocl
+{  
+
+///
+/// \brief CL point filter base class
+///
+class CLFilter { // virtual base class for filters
+    public:
+        /// constructor
+        CLFilter() {} ;
+        virtual ~CLFilter() {};
+        
+        /// add CLPoint
+        virtual void addCLPoint(const CLPoint& p) = 0;
+        virtual void setTolerance(const double tol) = 0;
+        
+        /// run filter
+        virtual void run() = 0;
+        
+        /// return CL-points to Python
+        virtual boost::python::list getCLPoints() = 0;
+        
+        /// the list of CL-points to be processed
+        std::list<CLPoint> clpoints;
+        /// tolerance
+        double tol;
+};
+
+/// find colinear points and delete redundant ones
+class LineCLFilter : public CLFilter  { 
+    public:
+        LineCLFilter();
+        ~LineCLFilter(){}; 
+        void addCLPoint(const CLPoint& p); 
+        void setTolerance(const double tol);
+        void run();
+        boost::python::list getCLPoints(); 
+
+};
+
+} // end namespace
+#endif
+// end file clfilter.h
