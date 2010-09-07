b95cb90e85fc6b61334ed16c4e06c5b147a44934
Anders Wallin <anders.e.e.wallin@gmail.com>
2010-09-07 14:52:00 +0000

    the path python wrapper

---
+++ src/geo/path_py.h
@@ -0,0 +1,78 @@
+/*  $Id$
+ *
+ *  Copyright 2010 Anders Wallin (anders.e.e.wallin "at" gmail.com)
+ *
+ *  This file is part of OpenCAMlib.
+ *
+ *  OpenCAMlib is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  OpenCAMlib is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with OpenCAMlib.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef PATH_PY_H
+#define PATH_PY_H
+
+#include <list>
+
+#include <boost/python.hpp>
+#include <boost/foreach.hpp>
+
+#include "path.h"
+
+
+
+namespace ocl
+{
+
+/// Python wrapper for Path
+class Path_py : public Path {
+    public:
+        Path_py() : Path () {};
+        Path_py(const Path &p) : Path(p) {};
+        
+        /// return the span-list to python
+        boost::python::list getSpans() {
+            boost::python::list slist;
+            BOOST_FOREACH(Span* span, span_list) {
+                        if(span->type() == LineSpanType)slist.append(((LineSpan*)span)->line);
+                        else if(span->type() == ArcSpanType)slist.append(((ArcSpan*)span)->arc);
+            }
+            return slist;
+        };
+
+        /// return a list of type/span pairs
+        boost::python::list getTypeSpanPairs() {
+            boost::python::list slist;
+            BOOST_FOREACH(Span* span, span_list) {
+                        if(span->type() == LineSpanType)
+                        {
+                            boost::python::list tuple;
+                            tuple.append(span->type());
+                            tuple.append(((LineSpan*)span)->line);
+                            slist.append(tuple);
+                        }
+                        else if(span->type() == ArcSpanType)
+                        {
+                            boost::python::list tuple;
+                            tuple.append(span->type());
+                            tuple.append(((ArcSpan*)span)->arc);
+                            slist.append(tuple);
+                        }
+            }
+            return slist;
+        };
+    
+    
+};
+
+} // end namespace
+#endif
+// end file path.h
