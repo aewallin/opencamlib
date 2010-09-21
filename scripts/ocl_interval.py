5e16acceb1e107b97d47b7657d37b36148984ffa
Anders Wallin <anders.e.e.wallin@gmail.com>
2010-09-21 13:54:18 +0000

    rename examples

--- scripts/ocl_interval.py
+++ scripts/ocl_interval.py
@@ -12,11 +12,13 @@ if __name__ == "__main__":
     print i
     i = ocl.Interval(0.123, 0.456)
     print i
-    i.updateLower(-0.222)
+    cc = ocl.CCPoint(1,2,3)
+    cc.type = ocl.CCType.VERTEX
+    i.updateLower(-0.222,cc)
     print i
-    i.updateLower(0)
+    i.updateLower(0,cc)
     print i
-    i.updateUpper(0)
+    i.updateUpper(0,cc)
     print i
-    i.updateUpper(2)
+    i.updateUpper(2,cc)
     print i
