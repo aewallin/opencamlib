e8b486431fb1ccb206bedac2275ac39d302e4968
Anders Wallin <anders.e.e.wallin@gmail.com>
2010-10-07 15:05:12 +0000

    simple qt and vtk example from the vtk wiki

---
+++ cpp_examples/qt_vtk_renderwindow/RenderWindow.cpp
@@ -0,0 +1,51 @@
+#include <QApplication>
+ 
+#include <vtkSmartPointer.h>
+#include <vtkSphereSource.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkActor.h>
+#include <vtkImageViewer.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkInteractorStyleImage.h>
+#include <vtkRenderer.h>
+#include <vtkJPEGReader.h>
+ 
+#include <QVTKWidget.h>
+ 
+int main(int argc, char** argv)
+{
+  QApplication app(argc, argv);
+ 
+  QVTKWidget widget;
+  widget.resize(256,256);
+ 
+  //setup sphere
+  vtkSmartPointer<vtkSphereSource> sphereSource = 
+      vtkSmartPointer<vtkSphereSource>::New();
+  sphereSource->Update();
+  vtkSmartPointer<vtkPolyDataMapper> sphereMapper = 
+      vtkSmartPointer<vtkPolyDataMapper>::New();
+  sphereMapper->SetInputConnection(sphereSource->GetOutputPort());
+  vtkSmartPointer<vtkActor> sphereActor = 
+      vtkSmartPointer<vtkActor>::New();
+  sphereActor->SetMapper(sphereMapper);
+ 
+  //setup window
+  vtkSmartPointer<vtkRenderWindow> renderWindow = 
+      vtkSmartPointer<vtkRenderWindow>::New();
+ 
+  //setup renderer
+  vtkSmartPointer<vtkRenderer> renderer = 
+      vtkSmartPointer<vtkRenderer>::New();
+  renderWindow->AddRenderer(renderer);
+ 
+  renderer->AddActor(sphereActor);
+  renderer->ResetCamera();
+ 
+  widget.SetRenderWindow(renderWindow);
+  widget.show();
+ 
+  app.exec();
+ 
+  return EXIT_SUCCESS;
+}
