b0d6f7ce88d75900289ea9485ddbee9c4c0f9047
Anders Wallin <anders.e.e.wallin@gmail.com>
2010-10-21 21:51:53 +0000

    waterline example

--- scripts/fiber_13_onetri_cutter_comparison.py
+++ scripts/fiber_13_onetri_cutter_comparison.py
@@ -10,7 +10,7 @@ def drawPoints(myscreen, clpoints, ccpoints):
     c.SetPoints()
     myscreen.addActor(c )
 
-def drawFiber(myscreen, f, fibercolor=camvtk.red):
+def drawFiber(myscreen, f, fibercolor):
     inter = f.getInts()
     print "fiber has ", len(inter) , " intervals"
     for i in inter:
@@ -30,7 +30,7 @@ def drawFiber(myscreen, f, fibercolor=camvtk.red):
             #c2 = camvtk.Circle(center=(ip2.x,ip2.y,ip2.z), radius = 0.3/2, color=fibercolor)
             #myscreen.addActor(c2)
 
-def drawFiber_clpts(myscreen, f, fibercolor=camvtk.red):
+def drawFiber_clpts(myscreen, f, clcolor):
     inter = f.getInts()
     #print "fiber has ", len(inter) , " intervals"
     for i in inter:
@@ -38,14 +38,16 @@ def drawFiber_clpts(myscreen, f, fibercolor=camvtk.red):
             ip1 = f.point( i.lower )
             ip2 = f.point( i.upper )
             #myscreen.addActor( camvtk.Line(p1=(ip1.x,ip1.y,ip1.z),p2=(ip2.x,ip2.y,ip2.z), color=fibercolor) )
-            myscreen.addActor( camvtk.Sphere(center=(ip1.x,ip1.y,ip1.z),radius=0.005, color=camvtk.clColor( i.lower_cc) ) )
-            myscreen.addActor( camvtk.Sphere(center=(ip2.x,ip2.y,ip2.z),radius=0.005, color=camvtk.clColor( i.upper_cc) ) )
+            sphcolor = clcolor 
+            myscreen.addActor( camvtk.Sphere(center=(ip1.x,ip1.y,ip1.z),radius=0.005, color=sphcolor ) )
+            sphcolor = clcolor 
+            myscreen.addActor( camvtk.Sphere(center=(ip2.x,ip2.y,ip2.z),radius=0.005, color=sphcolor ) )
             #cc1 = i.lower_cc
             #cc2 = i.upper_cc
             #myscreen.addActor( camvtk.Sphere(center=(cc1.x,cc1.y,cc1.z),radius=0.005, color=camvtk.pink ) )
             #myscreen.addActor( camvtk.Sphere(center=(cc2.x,cc2.y,cc2.z),radius=0.005, color=camvtk.pink ) )
 
-def yfiber(yvals,t,zh,myscreen):
+def yfiber(yvals,t,zh,myscreen,cutter,color):
     for y in yvals:
         f1 = ocl.Point(-0.5,y,zh) # start point of fiber
         f2 = ocl.Point(1.5,y,zh)  # end point of fiber
@@ -55,9 +57,9 @@ def yfiber(yvals,t,zh,myscreen):
         cutter.facetPush(f,i,t)
         cutter.edgePush(f,i,t)
         f.addInterval(i)
-        drawFiber_clpts(myscreen, f, camvtk.red)
+        drawFiber_clpts(myscreen, f, color)
 
-def xfiber(xvals,t,zh,myscreen):
+def xfiber(xvals,t,zh,myscreen,cutter,color):
     for x in xvals:
         f1 = ocl.Point(x,-0.5,zh) # start point of fiber
         f2 = ocl.Point(x,1.5,zh)  # end point of fiber
@@ -67,13 +69,24 @@ def xfiber(xvals,t,zh,myscreen):
         cutter.facetPush(f,i,t)
         cutter.edgePush(f,i,t)
         f.addInterval(i)
-        drawFiber_clpts(myscreen, f, camvtk.lblue)
+        drawFiber_clpts(myscreen, f, color)
 
-
+def oneCutterWaterline(myscreen, cutter, zh, color):
+    range=2
+    Nmax = 100
+    yvals = [float(n-float(Nmax)/2)/Nmax*range for n in xrange(0,Nmax+1)]
+    xvals = [float(n-float(Nmax)/2)/Nmax*range for n in xrange(0,Nmax+1)]
+    yfiber(yvals,t,zh,myscreen,cutter, color)
+    xfiber(xvals,t,zh,myscreen,cutter, color)
 
 if __name__ == "__main__":  
     print ocl.revision()
     myscreen = camvtk.VTKScreen()
+    myscreen.camera.SetPosition(0.5, 3, 2)
+    myscreen.camera.SetFocalPoint(0.5, 0.5, 0)
+    camvtk.drawArrows(myscreen,center=(-0.5,-0.5,-0.5))
+    camvtk.drawOCLtext(myscreen)
+    
     a = ocl.Point(0,1,0.3)
     b = ocl.Point(1,0.5,0.0)    
     c = ocl.Point(0.1,0.1,0.0)
@@ -87,46 +100,33 @@ if __name__ == "__main__":
     angle = math.pi/4
     diameter=0.3
     length=5
-    #cutter = ocl.BallCutter(diameter, length)
-    #cutter = ocl.CylCutter(diameter, length)
-    cutter = ocl.BullCutter(diameter, diameter/4, length)
+    cutter1 = ocl.BallCutter(diameter, length)
+    cutter2 = ocl.CylCutter(diameter, length)
+    cutter3 = ocl.BullCutter(diameter, diameter/4, length)
     #cutter = ocl.ConeCutter(diameter, angle, length)
     #cutter = cutter.offsetCutter( 0.1 )
     
-    print "cutter= ", cutter
-    print "lengt=", cutter.getLength()
-    print "fiber..."
+    
     range=2
-    Nmax = 100
+    Nmax = 50
     yvals = [float(n-float(Nmax)/2)/Nmax*range for n in xrange(0,Nmax+1)]
     xvals = [float(n-float(Nmax)/2)/Nmax*range for n in xrange(0,Nmax+1)]
-    zmin = -0.2082
+    zmin = 0.2082
     zmax = 0.3115
-    zNmax = 20
+    zNmax = 2
     dz = (zmax-zmin)/(zNmax-1)
     zvals=[]
     for n in xrange(0,zNmax):
         zvals.append(zmin+n*dz)
-    #zvals = [ float(n-float(zNmax)/2)/zNmax*range for n in xrange(0,zNmax+1)]
-    #print zvals
-    #exit()
-    #cc = ocl.CCPoint()
-    #zh = -0.1
-    #zh = -0.1571567
-    #zvals.append(zh)
+
     for zh in zvals:
-        yfiber(yvals,t,zh,myscreen)
-        xfiber(xvals,t,zh,myscreen)
+        oneCutterWaterline(myscreen, cutter1, zh, camvtk.yellow)
+        oneCutterWaterline(myscreen, cutter2, zh, camvtk.cyan)
+        oneCutterWaterline(myscreen, cutter3, zh, camvtk.mag)
+        #yfiber(yvals,t,zh,myscreen)
+        #xfiber(xvals,t,zh,myscreen)
     
     print "done."
-    
-    myscreen.camera.SetPosition(0.5, 3, 2)
-    myscreen.camera.SetFocalPoint(0.5, 0.5, 0)
-    
-    camvtk.drawArrows(myscreen,center=(-0.5,-0.5,-0.5))
-
-    camvtk.drawOCLtext(myscreen)
-    
     
     myscreen.render()    
     w2if = vtk.vtkWindowToImageFilter()
