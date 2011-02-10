b8af8532b015a335ea8397e907e7e7029d86444d
Anders Wallin <anders.e.e.wallin@gmail.com>
2011-02-10 10:12:59 +0000

    vis

--- scripts/voronoi_bisectors.py
+++ scripts/voronoi_bisectors.py
@@ -18,23 +18,84 @@ def drawEdge(myscreen, e, edgeColor=camvtk.yellow):
 def drawCircle(myscreen, c, circleColor):
     myscreen.addActor( camvtk.Circle( center=(c.c.x,c.c.y,c.c.z), radius=c.r, color=circleColor ) )
 
-
-
+def drawLine(myscreen, l, lineColor):
+    # a x + b y + c = 0
+    # x = -c/a
+    p1 = 100*ocl.Point( -l.c/l.a , 0 )
+    p2 = 100*ocl.Point( 0, -l.c/l.b )
+    myscreen.addActor( camvtk.Line( p1=( p1.x,p1.y,p1.z), p2=(p2.x,p2.y,p2.z), color=lineColor ) )
+    
 # CIRCLE def
 # (x(t) - xc1)^2 + (y(t)-yc1)^2 = (r1+k1*t)^2 
 class Circle:
     def __init__(self,c=ocl.Point(0,0),r=1,cw=1):
         self.c = c
         self.r = r
-        self.cw = cw
+        self.cw = cw # CW=1, CCW = -1
+        # k +1 enlarging circle
+        # k -1 shrinking circle 
 
 # LINE def
 # a1 x + b1 y + c1 + k1 t = 0 and a*a + b*b = 1 
 class Line:
-    def __init__(self,a,b,c):
-        self.a=a
-        self.b=b
-        selc.c=c
+    def __init__(self,a,b,c,k):
+        self.a = a
+        self.b = b
+        self.c = c
+        self.k = k # offset to left or right of line
+
+class LineLine:
+    def __init__(self,l1,l2):
+        self.delta= l1.a*l2.b-l2.a*l1.b
+        self.alfa1 = (l1.b*l2.c-l2.b*l1.c) / self.delta
+        self.alfa2 = (l2.a*l1.c-l1.a*l2.c) / self.delta
+        self.alfa3 = l2.b*l1.k-l1.b*l2.k
+        self.alfa4 = l1.a*l2.k-l2.a*l1.k
+    def getX(self):
+        x = []
+        x.append( self.alfa1 )
+        x.append( 0 )
+        x.append( -self.alfa3 )
+        x.append( 0 )
+        x.append( 0 )
+        x.append( 0 )
+        x.append( 0 )
+        x.append( 0 )
+        return x
+    def getY(self):
+        y = []
+        y.append( self.alfa2 )
+        y.append( 0 )
+        y.append( -self.alfa4 )
+        y.append( 0 )
+        y.append( 0 )
+        y.append( 0 )
+        y.append( 0 )
+        y.append( 0 )
+        return y
+
+# CIRCLE/LINE (same as point-line?)
+#    * alfa1= a2
+#    * alfa2= b2
+#    * alfa3= a2*xc1 + b2*yc1+d2 (c2?)
+#    * alfa4= r1 
+#       x1 = xc1
+#       x2 = alfa1*alfa3
+#       x3 = -alfa1, 
+#       x3 = alfa2, 
+#       x5 = alfa4, 
+#       x6 = lambda1, 
+#       x7 = alfa3, 
+#       x8 = -1
+#       y1 = yc1, 
+#       y2 = alfa2*alfa3, 
+#       y3 = -alfa2, 
+#       y4 = alfa1, 
+#       y5 = alfa4, 
+#       y6 = lambda1, 
+#       y7 = alfa3, 
+#       y8 = -1 
+
 
 class CircleCircle:
     # CIRCLE / CIRCLE
@@ -51,9 +112,8 @@ class CircleCircle:
             self.alfa2 = (c2.c.y-c1.c.y)/self.d
             self.alfa3 = (c2.r*c2.r-c1.r*c1.r-self.d*self.d)/(2*self.d)
             self.alfa4 = (c2.cw*c2.r-c1.cw*c1.r)/self.d
-
-
         self.c1 = c1 # store all of c1 also??
+
     def getX(self):
         x = []
         x.append( self.c1.c.x )
@@ -76,7 +136,7 @@ class CircleCircle:
         y.append( self.alfa3 )
         y.append( self.alfa4 )
         return y
-        
+
 class Bisector:
     def __init__(self, Bis):
         self.x= Bis.getX()
@@ -112,7 +172,7 @@ class Bisector:
         
 def drawBisector(myscreen, bis):
     N = 300
-    t= bis.minT()
+    t= 0
     tmax = 400
     dt = float(tmax)/float(N)
     ppts = []
@@ -125,15 +185,7 @@ def drawBisector(myscreen, bis):
         drawVertex(myscreen, p, camvtk.green, rad=1)
     for p in mpts:
         drawVertex(myscreen, p, camvtk.red, rad=1)
-        
-        
-# LINE/LINE
 
-# LINE/CIRCLE
-
-
-
-    
 if __name__ == "__main__":  
     print ocl.revision()
     myscreen = camvtk.VTKScreen()
@@ -150,10 +202,19 @@ if __name__ == "__main__":
     c2 = Circle(c=ocl.Point(20,30), r=60, cw=1)
     drawCircle(myscreen, c1, camvtk.cyan)
     drawCircle(myscreen, c2, camvtk.cyan)
-    
     c1c2 = CircleCircle(c1,c2)
-    bi = Bisector( c1c2 )
-    drawBisector( myscreen,bi )
+    bicc = Bisector( c1c2 )
+    drawBisector( myscreen, bicc )
+    
+    l1 = Line( math.cos(1),   math.sin(1)   , 1 , -1)
+    l2 = Line( math.cos(0.1), math.sin(0.1) , -1, 1)
+    drawLine(myscreen, l1, camvtk.yellow )
+    drawLine(myscreen, l2, camvtk.yellow )
+    
+    l1l2 = LineLine(l1,l2)
+    bill = Bisector( l1l2 )
+    drawBisector( myscreen, bill )
+    
     myscreen.render()
     
     #w2if.Modified() 
