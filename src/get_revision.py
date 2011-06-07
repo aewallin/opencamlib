b6dd4a2b72041ce1f8ce400ba08222a5f2a09320
Anders Wallin <anders.e.e.wallin@gmail.com>
2011-06-07 13:17:46 +0000

    make the revision number work for out-of-source builds

--- src/get_revision.py
+++ src/get_revision.py
@@ -1,10 +1,23 @@
 import commands
-import subprocess
+# import subprocess
+import sys
+# import getopt
+import os
 
 f = open("revision.h", "w")
+# print sys.argv
 
+# change directory to src/ 
+os.chdir(sys.argv[1])
+
+# run svnversion to get the revision number
 rev_number =  commands.getoutput("svnversion") 
+print "get_revision.py: got revision: ",rev_number
+
+# commands is deprecated, should use subprocess instead?
 # rev_number = subprocess.Popen(["svnversion"], stdout=subprocess.PIPE).communicate()[0] 
+# current_dir =  commands.getoutput("pwd") 
+# print "get_revision.py: writing revision.h to ", current_dir
 
 rev_string = '#define OCL_REV_STRING "OpenCAMLib Revision ' + rev_number + '"'
 
