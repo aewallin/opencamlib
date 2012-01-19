0e60d00c0b320ed21aef3f1eda91ade46834d3a6
Anders Wallin <anders.e.e.wallin@gmail.com>
2012-01-19 20:35:02 +0200

    change how python install dir is found. use relative path.

--- scripts/batchdropcutter_mtrush.py
+++ scripts/batchdropcutter_mtrush.py
@@ -7,7 +7,7 @@ import datetime
 import math
 
 if __name__ == "__main__":  
-    print ocl.revision()
+    print ocl.version()
     myscreen = camvtk.VTKScreen()
     
     #stl = camvtk.STLSurf("../stl/gnu_tux_mod.stl")
