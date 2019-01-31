971eb90ca0d35d8e9cdf46c01403411a1468186c
vespakoen <k.schmeets@gmail.com>
2019-01-31 13:29:01 +0100

    Emscripten fixes

--- src/emscriptenlib/build.sh
+++ src/emscriptenlib/build.sh
@@ -1,16 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
+source ~/Projects/emsdk/emsdk_env.sh
 rm -rf ../../buildemscripten || true
 mkdir ../../buildemscripten
 cd ../../buildemscripten
 emcmake cmake .. \
     -DCMAKE_BUILD_TYPE=Release \
-    -DBUILD_CPP_LIB="OFF" \
+    -DBUILD_CXX_LIB="OFF" \
     -DBUILD_EMSCRIPTEN_LIB="ON" \
     -DUSE_OPENMP="OFF" \
     -DBoost_INCLUDE_DIR="/usr/local/Cellar/boost/1.68.0/include"
 emmake make -j4
-cp src/opencamlib.* ../src/emscriptenlib
-cd ../src/emscriptenlib
-./node_modules/.bin/browserify test.src.js > test.js
-./node_modules/.bin/browserify index.js > pkg/index.js
+cp src/opencamlib.* ../src/npmpackage/
