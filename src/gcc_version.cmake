c3f3555270024104c51b27c33ecc7a293aae5dff
Anders Wallin <anders.e.e.wallin@gmail.com>
2019-07-26 11:18:37 +0300

    minor

--- src/gcc_version.cmake
+++ src/gcc_version.cmake
@@ -7,7 +7,7 @@ if(CMAKE_COMPILER_IS_GNUCXX)
       ${CMAKE_CXX_COMPILER}
       ARGS                    --version
       OUTPUT_VARIABLE _compiler_output)
-	  string(REGEX REPLACE ".*([0-9]\\.[0-9]\\.[0-9]).*" "\\1"
+      string(REGEX REPLACE ".*([0-9]\\.[0-9]\\.[0-9]).*" "\\1"
       gcc_compiler_version 
       "${_compiler_output}"
       )
@@ -15,11 +15,11 @@ if(CMAKE_COMPILER_IS_GNUCXX)
   message(STATUS "C++ compiler version: ${gcc_compiler_version} [${CMAKE_CXX_COMPILER}]")
   
   IF(${gcc_compiler_version} MATCHES ".*4\\.[6789]\\.[0-9].*")
-	#MESSAGE("gcc >= 4.6")
-	set(GCC_4_6 TRUE)
+    #MESSAGE("gcc >= 4.6")
+    set(GCC_4_6 TRUE)
   ELSE(${gcc_compiler_version} MATCHES ".*4\\.[6789]\\.[0-9].*")
-	#MESSAGE("gcc < 4.6")
-	set(GCC_4_6 FALSE)
+    #MESSAGE("gcc < 4.6")
+    set(GCC_4_6 FALSE)
   ENDIF(${gcc_compiler_version} MATCHES ".*4\\.[6789]\\.[0-9].*")
 
 endif(CMAKE_COMPILER_IS_GNUCXX)
