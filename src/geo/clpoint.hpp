96fdebf2c50970b7d9c7cf92408c6c8897ff3d4d
Gabriel Wicke <gswicke@gmail.com>
2020-05-17 22:54:20 -0700

    Protect against race in CLPoint::liftZ
    Since moving to a machine with 12 hw threads, I have seen occasional
    segfaults from a double free in CLPoint::liftZ. This is consistent with
    several threads calling liftZ on the same CLPoint instance, racing to
    delete the previous instance.
    
    To avoid the race, this diff wraps the raw pointer in a std::atomic, and
    then uses std::atomic<T>::compare_exchange_strong for an atomic
    replacement without double frees or leaks.
    
    Another option would be to use a mutex.

--- src/geo/clpoint.hpp
+++ src/geo/clpoint.hpp
@@ -1,26 +1,27 @@
 /*  $Id$
- * 
+ *
  *  Copyright (c) 2010 Anders Wallin (anders.e.e.wallin "at" gmail.com).
- *  
- *  This file is part of OpenCAMlib 
+ *
+ *  This file is part of OpenCAMlib
  *  (see https://github.com/aewallin/opencamlib).
- *  
+ *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as published by
  *  the Free Software Foundation, either version 2.1 of the License, or
  *  (at your option) any later version.
- *  
+ *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU Lesser General Public License for more details.
- *  
+ *
  *  You should have received a copy of the GNU Lesser General Public License
  *  along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 #ifndef CLPOINT_H
 #define CLPOINT_H
 
+#include <atomic>
 #include <string>
 #include <iostream>
 
@@ -47,26 +48,27 @@ class CLPoint : public Point {
         /// cl-point at Point p
         CLPoint(const Point& p);
         virtual ~CLPoint();
-        /// Pointer to the corresponding CCPoint
-        CCPoint* cc; 
+        /// Atomic pointer to the corresponding CCPoint, protected against
+        /// concurrent replacement in liftZ.
+        std::atomic<CCPoint*> cc;
         /// string repr
         std::string str() const;
-        
+
         /// if cc is in the edge p1-p2, test if clpoint needs to be lifted to z
         /// if so, set cc = cc_tmp and return true
         bool liftZ_if_InsidePoints(double z, CCPoint& cc_tmp, const Point& p1,const Point& p2);
-        
+
         /// if cc in in Triangle facet, test if clpoint needs to be lifted
         /// if so, set cc=cc_tmp and return true
         bool liftZ_if_inFacet(double z, CCPoint& cc_tmp, const Triangle& t);
-        
-        /// if zin > z, lift CLPoint and update cc-point, and return true 
+
+        /// if zin > z, lift CLPoint and update cc-point, and return true
         bool liftZ(double zin, CCPoint& ccp);
-        
+
         /// if zin > z, lift CLPoint and return true.
         bool liftZ(const double zin);
-        
-        
+
+
         /// return true if cl-point above triangle
         bool below(const Triangle& t) const;
         /// return the CCPoint (for python)
