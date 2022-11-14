7507a2190c046e98c0f3d01a5a0479428fdd461c
Koen Schmeets <hello@koenschmeets.nl>
2022-11-14 14:51:17 +0100

    tmpfix: compile boost from source on ubuntu to an easily findable location

--- scripts/install-ubuntu.sh
+++ scripts/install-ubuntu.sh
@@ -19,4 +19,17 @@ if [ "$1" = "emscriptenlib" ]; then
     cd emsdk
     ./emsdk install latest
     ./emsdk activate latest
+
+    # we will compile boost from source because find_package(Boost) is not working with emscripten
+    # even with CMAKE_FIND_ROOT_PATH_MODE_INCLUDE=BOTH, CMAKE_FIND_ROOT_PATH_MODE_PACKAGE=BOTH
+    # and by setting CMAKE_FIND_ROOT_PATH=/ the library is not found, will investigate this later
+    # and compile from source for now
+    mkdir /tmp/boost
+    cd /tmp/boost
+    wget -nv -O boost.tar.gz https://boostorg.jfrog.io/artifactory/main/release/1.80.0/source/boost_1_80_0.tar.gz
+    tar -zxf boost.tar.gz
+
+    cd boost_1_80_0
+    sudo ./bootstrap.sh --prefix=/usr/local
+    sudo ./b2 address-model=64 threading=multi -j2 variant=$2 link=static --layout=system cxxflags='-fPIC' install
 fi
