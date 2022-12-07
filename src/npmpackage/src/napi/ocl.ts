734070c949300a1e7a9a9ebbae5f9df20f7434cc
Koen Schmeets <hello@koenschmeets.nl>
2022-12-07 22:30:55 +0100

    Fix arch not propagating correctly, fix node.js lib issue, bump

--- src/npmpackage/src/napi/ocl.ts
+++ src/npmpackage/src/napi/ocl.ts
@@ -3,7 +3,7 @@ if (process.env.DEBUG) {
     buildType = 'Debug'
 }
 
-const platform = process.platform === 'darwin' ? 'darwin' : (process.platform === 'win32' ? 'windows' : 'linux')
+const platform = process.platform === 'darwin' ? 'macos' : (process.platform === 'win32' ? 'windows' : 'linux')
 
 const oclLib = require(__dirname + '/../../build/' + buildType + '/' + platform + '-nodejs-' + process.arch + '/ocl.node')
 
