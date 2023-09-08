f89b7c574974b23719a41fff9e941235d633ddf2
Koen Schmeets <hello@koenschmeets.nl>
2023-09-08 21:23:26 +0200

    Move install_ci_deps to seperate script

---
+++ .github/scripts/ci_deps.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+command_exists() {
+    command -v "${1}" >/dev/null 2>&1;
+}
+
+is_root() {
+    [ "${EUID:-$(id -u)}" -eq 0 ];
+}
+
+install_ci_dependencies() {
+    if [ "${determined_os}" = "windows" ]; then
+        if command_exists choco; then
+            choco install curl --no-progress
+        fi
+    elif [ "${determined_os}" = "linux" ]; then
+        if command_exists apt; then
+            if ! is_root; then
+                maybe_sudo="sudo"
+            fi
+            ${maybe_sudo} apt update
+            ${maybe_sudo} apt install -y --no-install-recommends curl
+        elif command_exists yum; then
+            if ! is_root; then
+                maybe_sudo="sudo"
+            fi
+            ${maybe_sudo} yum install curl
+        fi
+    elif [ "${determined_os}" = "macos" ]; then
+        prettyprint "Downloading libomp for: " "${OCL_MACOS_ARCHITECTURE}"
+        if [ "${OCL_MACOS_ARCHITECTURE}" = "arm64" ]; then
+            libomp_tar_loc=$(brew fetch --bottle-tag=arm64_big_sur libomp | grep -i downloaded | grep tar.gz | cut -f2 -d ":" | xargs echo)
+        else
+            libomp_tar_loc=$(brew fetch --bottle-tag=big_sur libomp | grep -i downloaded | grep tar.gz | cut -f2 -d ":" | xargs echo)
+        fi
+        temp_dir="/tmp"
+        cp "${libomp_tar_loc}" "${temp_dir}/libomp.tar.gz"
+        mkdir "${temp_dir}/libomp" || true
+        tar -xzf "${temp_dir}/libomp.tar.gz" -C "${temp_dir}/libomp"
+        libomp_prefix=$(find "${temp_dir}/libomp/libomp" -depth 1 | head -1)
+        export OPENMP_PREFIX_MACOS="${temp_dir}/libomp/libomp/fixed"
+        mv "${libomp_prefix}" "${OPENMP_PREFIX_MACOS}"
+    fi
+}
--- install.sh
+++ install.sh
@@ -186,45 +186,6 @@ command_exists() {
     command -v "${1}" >/dev/null 2>&1;
 }
 
-is_root() {
-    [ "${EUID:-$(id -u)}" -eq 0 ];
-}
-
-install_ci_dependencies() {
-    if [ "${determined_os}" = "windows" ]; then
-        if command_exists choco; then
-            choco install curl --no-progress
-        fi
-    elif [ "${determined_os}" = "linux" ]; then
-        if command_exists apt; then
-            if ! is_root; then
-                maybe_sudo="sudo"
-            fi
-            ${maybe_sudo} apt update
-            ${maybe_sudo} apt install -y --no-install-recommends curl
-        elif command_exists yum; then
-            if ! is_root; then
-                maybe_sudo="sudo"
-            fi
-            ${maybe_sudo} yum install curl
-        fi
-    elif [ "${determined_os}" = "macos" ]; then
-        prettyprint "Downloading libomp for: " "${OCL_MACOS_ARCHITECTURE}"
-        if [ "${OCL_MACOS_ARCHITECTURE}" = "arm64" ]; then
-            libomp_tar_loc=$(brew fetch --bottle-tag=arm64_big_sur libomp | grep -i downloaded | grep tar.gz | cut -f2 -d ":" | xargs echo)
-        else
-            libomp_tar_loc=$(brew fetch --bottle-tag=big_sur libomp | grep -i downloaded | grep tar.gz | cut -f2 -d ":" | xargs echo)
-        fi
-        temp_dir="/tmp"
-        cp "${libomp_tar_loc}" "${temp_dir}/libomp.tar.gz"
-        mkdir "${temp_dir}/libomp" || true
-        tar -xzf "${temp_dir}/libomp.tar.gz" -C "${temp_dir}/libomp"
-        libomp_prefix=$(find "${temp_dir}/libomp/libomp" -depth 1 | head -1)
-        export OPENMP_PREFIX_MACOS="${temp_dir}/libomp/libomp/fixed"
-        mv "${libomp_prefix}" "${OPENMP_PREFIX_MACOS}"
-    fi
-}
-
 download_boost() {
     if [ ! -f "${TMPDIR:-"/tmp"}/boost.tar.gz" ]; then
         prettyprint "Downloading boost.tar.gz"
@@ -532,11 +493,6 @@ test_emscriptenlib() {
 if [ -n "${OCL_INSTALL_SYSTEM_DEPS}" ]; then
     prettyprint "Installing system dependencies..."
     install_system_dependencies
-fi
-
-if [ -n "${OCL_INSTALL_CI_DEPS}" ]; then
-    prettyprint "Installing ci dependencies..."
-    install_ci_dependencies
 fi
 
 if [ -n "${OCL_INSTALL_BOOST}" ]; then
