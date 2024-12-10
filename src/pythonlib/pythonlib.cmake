77d17fef5baa88584034ef020069ef798a328ee3
Koen Schmeets <hello@koenschmeets.nl>
2024-12-10 23:06:03 +0100

    Try to use FindBoost from config

--- src/pythonlib/pythonlib.cmake
+++ src/pythonlib/pythonlib.cmake
@@ -4,7 +4,7 @@ if(Python3_FOUND)
   message(STATUS "Python libraries: " ${Python3_LIBRARIES})
   message(STATUS "Python executable: " ${Python3_EXECUTABLE})
 endif()
-find_package(Boost COMPONENTS python${Python3_VERSION_MAJOR}${Python3_VERSION_MINOR} REQUIRED)
+find_package(Boost CONFIG COMPONENTS python${Python3_VERSION_MAJOR}${Python3_VERSION_MINOR} REQUIRED)
 
 # include dirs
 include_directories(${PROJECT_SOURCE_DIR}/cutters)
